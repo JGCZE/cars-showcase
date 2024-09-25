5e2c0ab87599c69c049e108d93d2b900cc4373d7
Jan Gross <jan.gross.cze@gmail.com>
2024-09-25 13:50:49 +0200

    Searching by brands, components UI, filtering

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,16 @@
 import { MouseEventHandler } from "react";
+import SearchManufacturer from "../components/SearchManufacturer";
 
 export interface CustomButtonProps {
   title: string;
   containerStyles: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+}
+
+export interface ISearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
 }
 
 export type IFooterLinks = {
