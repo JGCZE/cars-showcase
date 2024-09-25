a81a5203c48495995b564db9ae90468c921bbc95
Jan Gross <jan.gross.cze@gmail.com>
2024-09-25 19:30:04 +0200

    Car Cards, button modifications, search manufacturer

--- types/index.ts
+++ types/index.ts
@@ -6,11 +6,29 @@ export interface CustomButtonProps {
   containerStyles: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: string;
 }
 
 export interface ISearchManufacturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+
+export interface ICarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
 
 export type IFooterLinks = {
