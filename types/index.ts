fd86726b82a32232c1dec8a5e20b79ac909284c1
Jan Gross <jan.gross.cze@gmail.com>
2024-09-25 11:06:01 +0200

    Header and Footer layout! :wq q

--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,15 @@ export interface CustomButtonProps {
   title: string;
   containerStyles: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
 }
+
+export type IFooterLinks = {
+  title: string;
+  links: Array<Links>;
+};
+
+type Links = {
+  title: string;
+  url: string;
+};
